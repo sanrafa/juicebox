081d037c67667a0605eb2b5a1edce2c7220dbb68
Rafa Brown <shane.rafael@zohomail.com>
2021-10-29 15:52:15 -0600

    Create /register route and token verification

--- api/users.js
+++ api/users.js
@@ -1,7 +1,9 @@
+const jwt = require("jsonwebtoken");
+
 const express = require("express");
 const usersRouter = express.Router();
 
-const { getAllUsers, getUserByUsername } = require("../db");
+const { getAllUsers, getUserByUsername, createUser } = require("../db");
 
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
@@ -23,7 +25,8 @@ usersRouter.post("/login", async (req, res, next) => {
     const user = await getUserByUsername(username);
 
     if (user && user.password === password) {
-      res.send({ message: "you're logged in!" });
+      const token = jwt.sign(user, process.env.JWT_SECRET);
+      res.send({ message: "you're logged in!", token });
     } else {
       next({
         name: "IncorrectCredentialsError",
@@ -33,6 +36,46 @@ usersRouter.post("/login", async (req, res, next) => {
   } catch (err) {
     console.error(err);
     next(err);
+  }
+});
+
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "That username already exists.",
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    res.send({
+      message: "Thanks for signing up",
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
